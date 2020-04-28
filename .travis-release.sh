ee060d5e168bef82148f2bbd5c92d2049a79dafb
oscar-raig <oscar.raig@gmail.com>
2020-04-29 00:13:42 +0200

    fix(Root): fix travis-release
    Fixing travis-release script

--- .travis-release.sh
+++ .travis-release.sh
@@ -8,10 +8,11 @@ git config --global user.name "oscar-raig"
 # Remove existing "origin"
 git remote rm origin
 # Add new "origin" with access token in the git URL for authentication
-git remote add origin https://${GH_TOKEN}@github.com:oscar-raig/kikilo-uilib-components.git > /dev/null 2>&1
+git remote add origin https://oscar-raig:${GH_TOKEN}@github.com/oscar-raig/kikilo-uilib-components.git > /dev/null 2>&1
 # move to master before release
 git checkout master
 # get the latest master changes
 git pull origin master
 # release new packages
+echo "releasing"
 ./node_modules/.bin/sui-mono release
