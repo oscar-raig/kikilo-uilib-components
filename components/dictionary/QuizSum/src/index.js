ff1010d873421a6c04da1ff7c63c2f64f1300634
oscar-raig <oscar.raig@gmail.com>
2020-05-11 23:56:39 +0200

    feat(dictionary/QuizSum): using some funny sui components

--- components/dictionary/QuizSum/src/index.js
+++ components/dictionary/QuizSum/src/index.js
@@ -1,8 +1,23 @@
 import React, {useState, useContext} from 'react'
 import PropTypes from 'prop-types'
 import Context from '@s-ui/react-context'
-
+import {Thumbup, Thumbdown} from '@s-ui/react-icons'
+import AtomButton from '@s-ui/react-atom-button'
+import '@s-ui/react-atom-button/lib/index.scss'
+import AtomIcon, {
+  ATOM_ICON_COLORS,
+  ATOM_ICON_SIZES
+} from '@s-ui/react-atom-icon'
+import '@s-ui/react-atom-icon/lib/index.scss'
 export default function DictionaryQuizSum({digits = 1, title = 'SUM_NUMBERS'}) {
+  const Icon = (
+    <AtomIcon color={ATOM_ICON_COLORS.accent} size="large">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+      </svg>
+    </AtomIcon>
+  )
+
   const {i18n} = useContext(Context)
   const generateRandomNumber = () =>
     Math.floor(Math.random() * Math.pow(10, digits))
@@ -34,11 +49,11 @@ export default function DictionaryQuizSum({digits = 1, title = 'SUM_NUMBERS'}) {
       <div className="kik-Sum-Container">
         <h1>{i18n.t(title)}</h1>
         <div className="kik-Fail">
-          <span>Errades</span>
+          <Thumbdown fillColor="red" />
           <span> {fails} </span>
         </div>
         <div className="kik-Hit">
-          <span> Encerts </span>
+          <Thumbup fillColor="green" />
           <span> {hits} </span>
         </div>
         <div className="kik-Sum-first-operand">
@@ -61,14 +76,19 @@ export default function DictionaryQuizSum({digits = 1, title = 'SUM_NUMBERS'}) {
         </div>
         <div className="kik-buttons-container">
           <div className="kik-Sum-new-quiz">
-            <button className="kik-Sum-button" onClick={newQuizSum}>
+            <AtomButton className="kik-Sum-button" onClick={newQuizSum}>
               Nova Suma
-            </button>
+            </AtomButton>
           </div>
           <div className="kik-Sum-resolve">
-            <button className="kik-Sum-button" onClick={newQuizResolve}>
+            <AtomButton
+              size={ATOM_ICON_SIZES.extraLarge}
+              className="kik-Sum-button"
+              onClick={newQuizResolve}
+              rightIcon={Icon}
+            >
               Resol
-            </button>
+            </AtomButton>
           </div>
         </div>
       </div>
